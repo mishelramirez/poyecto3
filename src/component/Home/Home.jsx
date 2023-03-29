63c8ca0c6955d941a62e17f8c4e0efbb457363da
cintya <rmishel564@gmail.com>
2023-03-29 15:37:34 -0500

    se realiza cuadro 1 de la imagen principal

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -8,7 +8,161 @@ export default function Home() {
             <div className="container">
                 <div className="contaheader">
                     <div className="headerbarra"></div>
-                    <MenuIcon className='icon'/>
+                    <MenuIcon className='icon' />
+                </div>
+                <div className='container1' >
+
+                    <div className='circulo1'></div>
+
+                    <div className='rayas'>
+                        <div className='raya'></div>
+                        <div className='raya1'></div>
+                        <div className='raya2'></div>
+                        <div className='raya3'></div>
+                    </div>
+
+                    <div className='circulitos'>
+                        <div className='circulos1'></div>
+                        <div className='circulos2'></div>
+                        <div className='circulos3'></div>
+                        <div className='circulos4'></div>
+                        <div className='circulos5'></div>
+                    </div>
+
+                </div>
+                <div className='container1' >
+
+                    <div className='circulo2'></div>
+
+                    <div className='rayas'>
+                        <div className='raya'></div>
+                        <div className='raya1'></div>
+                        <div className='raya2'></div>
+                        <div className='raya3'></div>
+                    </div>
+
+                    <div className='circulitos'>
+                        <div className='circulos1'></div>
+                        <div className='circulos2'></div>
+                        <div className='circulos3'></div>
+                        <div className='circulos4'></div>
+                        <div className='circulos5'></div>
+                    </div>
+
+                </div>
+
+                <div className='container1' >
+
+                    <div className='circulo3'></div>
+
+                    <div className='rayas'>
+                        <div className='raya'></div>
+                        <div className='raya1'></div>
+                        <div className='raya2'></div>
+                        <div className='raya3'></div>
+                    </div>
+
+                    <div className='circulitos'>
+                        <div className='circulos1'></div>
+                        <div className='circulos2'></div>
+                        <div className='circulos3'></div>
+                        <div className='circulos4'></div>
+                        <div className='circulos5'></div>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+            <div className="container">
+                <div className="contaheader">
+                    <div className="headerbarra"></div>
+                    <MenuIcon className='icon' />
+                </div>
+                <div className='container1' >
+
+                    <div className='circulo1'></div>
+
+                    <div className='rayas'>
+                        <div className='raya'></div>
+                        <div className='raya1'></div>
+                        <div className='raya2'></div>
+                        <div className='raya3'></div>
+                    </div>
+                </div>
+                <div className='container1' >
+
+                    <div className='circulo2'></div>
+
+                    <div className='rayas'>
+                        <div className='raya'></div>
+                        <div className='raya1'></div>
+                        <div className='raya2'></div>
+                        <div className='raya3'></div>
+                    </div>
+                </div>
+
+                <div className='container1' >
+
+                    <div className='circulo3'></div>
+
+                    <div className='rayas'>
+                        <div className='raya'></div>
+                        <div className='raya1'></div>
+                        <div className='raya2'></div>
+                        <div className='raya3'></div>
+                    </div>
+
+
+                </div>
+                <div className='container1' >
+
+                    <div className='circulo1'></div>
+
+                    <div className='rayas'>
+                        <div className='raya'></div>
+                        <div className='raya1'></div>
+                        <div className='raya2'></div>
+                        <div className='raya3'></div>
+                    </div>
+                </div>
+                <div className='container1' >
+
+                    <div className='circulo2'></div>
+
+                    <div className='rayas'>
+                        <div className='raya'></div>
+                        <div className='raya1'></div>
+                        <div className='raya2'></div>
+                        <div className='raya3'></div>
+                    </div>
+                </div>
+
+                <div className='container1' >
+
+                    <div className='circulo3'></div>
+
+                    <div className='rayas'>
+                        <div className='raya'></div>
+                        <div className='raya1'></div>
+                        <div className='raya2'></div>
+                        <div className='raya3'></div>
+                    </div>
+
+
+                </div>
+                <div className='container1' >
+
+                    <div className='circulo1'></div>
+
+                    <div className='rayas'>
+                        <div className='raya'></div>
+                        <div className='raya1'></div>
+                        <div className='raya2'></div>
+                        <div className='raya3'></div>
+                    </div>
                 </div>
             </div>
         </div>
